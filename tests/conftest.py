cd86762f4a49a492bedb9606d2b62287e2204f44
YuliyaShkretova <129888062+YuliyaShkretova@users.noreply.github.com>
2023-05-29 16:04:13 +0200

    Mid level with updated structure

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,3 @@
-import webbrowser
-
 import pytest
 from selene.support.shared import browser
 
@@ -8,8 +6,8 @@ from selene.support.shared import browser
 def browser_setup():
     browser.config.driver_name = 'chrome'
     browser.config.base_url = 'https://demoqa.com'
-    browser.config.window_width = 903
-    browser.config.window_height = 1166
+    browser.config.window_width = 1280
+    browser.config.window_height = 1024
     browser.config.timeout = 10
 
     yield
